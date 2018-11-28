8cd9fb17d04dbb09378690f961070c107794a4fa
mathilde <mathilde@DESKTOP-658AR7A>
2018-11-28 15:24:52 +0100

    test 2

--- tpeseo/src/main/java/com/tpeseo/App.java
+++ tpeseo/src/main/java/com/tpeseo/App.java
@@ -1,5 +1,9 @@
 package com.tpeseo;
 
+import java.awt.List;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -7,6 +11,27 @@ package com.tpeseo;
 public class App 
 {
 	static String tableName = "";
+	static String ville = ""; 
+
+	public static final String ATT_MESSAGES = "messages";
+    public static final String VUE          = "/WEB-INF/test_jdbc.jsp";
+	
+	
+	private ArrayList<String> messages = new ArrayList<String>();
+
+    public ArrayList<String> executerTests( HttpServletRequest request ) {
+
+        return messages;
+    }
+
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        
+        request.setAttribute( ATT_MESSAGES, messages );
+
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+
 	
     public static void main( String[] args )
     {
