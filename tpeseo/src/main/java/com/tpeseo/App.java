0b61e396ca014f2e66531d1e86d5d5ec01e4f59a
mathilde <mathilde@DESKTOP-658AR7A>
2018-11-28 14:09:28 +0100

    essay de bout en bout

--- tpeseo/src/main/java/com/tpeseo/App.java
+++ tpeseo/src/main/java/com/tpeseo/App.java
@@ -6,8 +6,12 @@ package com.tpeseo;
  */
 public class App 
 {
+	static String tableName = "";
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //dropTable(tableName);
+    	//readSV(filePath);
     }
 }
